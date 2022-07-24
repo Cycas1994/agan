ca3725497572eeb2d44af2f8bdfe9c26a1cedb24
cycas <445729912@qq.com>
2022-07-24 16:45:33 +0800

    algs

---
+++ algs/src/test/java/com/cycas/algs/AlgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cycas.algs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
