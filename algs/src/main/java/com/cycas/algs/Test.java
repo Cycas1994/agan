2d5b447f69a04a442a129af2d1113f1deef3e8dd
那欣 <xin.na@theone.art>
2022-08-08 12:22:20 +0800

    fix

---
+++ algs/src/main/java/com/cycas/algs/Test.java
@@ -0,0 +1,40 @@
+package com.cycas.algs;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Scanner;
+
+/**
+ * @author NaXin
+ * @since 2022-08-07
+ */
+public class Test {
+    private static Scanner scanner;
+    public static void main(String[] args) {
+
+        boolean[][] arr = {{true, true, true}, {false, false, false}, {true, true, true}};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" ");
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print((j + 1) + " ");
+            }
+            break;
+        }
+        System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(i+1 + ":");
+            boolean[] arr2 = arr[i];
+            for (int j = 0; j < arr2.length; j++) {
+                String str = "";
+                if (arr[i][j]) {
+                    str = "* ";
+                } else {
+                    str = " ";
+                }
+                System.out.print(str);
+            }
+            System.out.println();
+        }
+    }
+}
